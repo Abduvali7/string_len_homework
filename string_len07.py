896fd9b684604ab2f605f451dd40563ee8376753
Abduvali7 <hamzaqulovagulmira@gmail.com>
2023-03-15 11:04:45 +0500

    07

--- string_len07.py
+++ string_len07.py
@@ -8,10 +8,21 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    if len([s1,s2])%2==0:
+    if len(s1)%2==0 and len(s2)%2==0 and len(s3)%2==0:
         return "[]"
-    elif len([s2,s3])%2==0:
-        return "[]"
-    elif len([s1,s3])%2==0:
-        return "[]"
+    elif len(s1)%2==1 and len(s2)%2==1 and len(s3)%2==1:
+        return f"[{s1}, {s2}, {s3}]"
+    elif len(s1)%2==1 and len(s2)%2==1:
+        return f"[{s1}, {s2}]"
+    elif len(s1)%2==1 and len(s3)%2==1:
+        return f"[{s1}, {s3}]"
+    elif len(s2)%2==1 and len(s3)%2==1:
+        return f"[{s2}, {s3}]"
+    elif len(s1)%2==1:
+        return f"[{s1}]"
+    elif len(s2)%2==1:
+        return f"[{s2}]"
+    elif len(s3)%2==1:
+        return f"[{s3}]"
+    
 print(main("school", "code", "book"))
